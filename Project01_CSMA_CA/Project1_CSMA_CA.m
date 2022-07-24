70ded71a5954843077abff5cdd37257e9e307c9e
Yufan Zhuang <87563152+egg988@users.noreply.github.com>
2022-07-24 17:02:57 +0800

    Delete Project1_CSMA_CA.m

--- Project01_CSMA_CA/Project1_CSMA_CA.m
+++
@@ -1,9 +0,0 @@
-userNums = [1:1:50];
-effis = zeros(1,length(userNums));
-for i = 1:length(userNums)
-    effis(i) = simulation(userNums(i));
-end
-plot(userNums,effis,'LineWidth',2);
-xlabel('Number of STAs');
-ylabel('Efficiency');
-        
